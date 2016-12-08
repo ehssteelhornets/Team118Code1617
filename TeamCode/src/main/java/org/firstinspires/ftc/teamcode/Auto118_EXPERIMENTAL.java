8182ef159177ff7cda2be4e1e19dec9691273653
ehssteelhornets <ehssteelhornets@gmail.com>
2016-12-08 15:33:10 -0500

    Edited TeleOp and Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto118_EXPERIMENTAL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto118_EXPERIMENTAL.java
@@ -17,10 +17,16 @@ public class Auto118_EXPERIMENTAL extends AutoMethods {
     }
 
    int opState = 1;
+    public void set_drive_power(double pwr) {
+        r$front.setPower(pwr);
+        r$rear.setPower(pwr);
 
+        l$front.setPower(pwr);
+        l$rear.setPower(pwr);
+    }
     @Override
     public void loop() {
-
+            telemetry.addData(""+opState,0);
         switch(opState) {
 
             case 1:     //Launch 2 balls - Works. Don't touch.
@@ -36,21 +42,23 @@ public class Auto118_EXPERIMENTAL extends AutoMethods {
                 shooter.setPower(0);
                 telemetry.addData("2", 2);
                 busySleep(10000);
-                opState++;
                 telemetry.addData("Checkpoint 1", 1);
 
                 while (!have_encoders_reset())  {
                     reset_drive_encoders();
+                    opState++;
                 }
                 break;
 
             case 2:
-                set_drive_power(.99);
-                if (have_encoders_reached(getNumTicks(70))) {
-                    set_drive_power(0);
-                    opState++;
-                    telemetry.addData("Tick Goal Reached", 0);
+
+                while (!have_encoders_reached(getNumTicks(70))) {
+                    set_drive_power(1);
                 }
+                set_drive_power(0);
+                opState++;
+                telemetry.addData("Tick Goal Reached", hsvr);
+
                 telemetry.addData("Checkpoint 2", 2);
 
                 break;
